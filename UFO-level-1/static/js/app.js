b7a0fe75fb64e775ae616455616f143deceae838
Josh <joshua.dickenson91@gmail.com>
2020-09-05 13:54:12 +0800

    Filter by Button works

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -24,17 +24,42 @@ tableData.forEach((report) => {
 
 //Selecting ID filter button and create the clicking event.
 var filterbutton = d3.select("#filter-btn");
-filterbutton.on("click", function() {
- 
+filterbutton.on("click", runfilters);
+//or pressing enter
+var form = d3.select("#form");
+form.on("submit", runfilters);
+
+//Create the function
+function runfilters() {
+
+
     //remove the current data from tbody
     d3.select("tbody").html("");
+
+    //Don't refresh the page
+    d3.event.preventDefault();
 
     //Get teh date in the filter box and store it
     var FilterDate = d3.select("#datetime").property("value");
     console.log(FilterDate);
 
+    //Filter the table data by the FilterDate varuiable
 
+    var filteredData = tableData.filter(record => record.datetime === FilterDate);
+    console.log(filteredData);
 
+    // Output the table by filtered date
+    filteredData.forEach((report) => {
+        var row = tbody.append('tr');
+
+        Object.entries(report).forEach(([key, value]) => {
+            console.log(key, value);
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    });
+
+}
 
 
 
